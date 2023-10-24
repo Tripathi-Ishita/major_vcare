e2bee79bf339cf57230b6b7541ead1f6ab0b9998
Tripathi-Ishita <ishitatripathi518@gmail.com>
2023-10-24 14:05:27 +0530

    jfb

--- lib/screen/testing.dart
+++ lib/screen/testing.dart
@@ -8,15 +8,52 @@ class Testing extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(500),
-            child: Container(
-              height: MediaQuery.of(context).size.height*0.3,
-              width: MediaQuery.of(context).size.width*0.6,
-              color: Color.fromRGBO(129, 71, 255, 1.0),
-
+          Positioned(
+             left: -10,
+              top: -30,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(500),
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  color: Colors.deepPurple,
+                ),
+              )),
+          Positioned(
+            right: -100,
+            top: -150,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(500),
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.5,
+                width: MediaQuery.of(context).size.width * 1,
+                color: Color.fromRGBO(129, 71, 255, 1.0),
+              ),
             ),
           ),
+          Positioned(
+             bottom: -20,
+              left: -40,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(500),
+                child: Container(
+                  height: MediaQuery.of(context).size.height * .15,
+                  width: MediaQuery.of(context).size.width * 0.3,
+                  color: Colors.deepPurple,
+                ),
+              )),
+          Positioned(
+              bottom: 40,
+              left: 80,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(500),
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.09,
+                  width: MediaQuery.of(context).size.width * 0.18,
+                  color: Colors.deepPurple,
+                ),
+              )),
+
         ],
       ),
     );
