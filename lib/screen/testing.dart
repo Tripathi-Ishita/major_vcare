a198b4dded80f6a726bf70eff522ad1350f5e85e
Ankit Kumar <akumar78210@gmail.com>
2023-10-31 15:45:20 +0530

    Update testing.dart

--- lib/screen/testing.dart
+++ lib/screen/testing.dart
@@ -11,7 +11,7 @@ class Testing extends StatelessWidget {
           child: Column(
             children: [
               Container(
-                height: MediaQuery.of(context).size.height * 0.35,
+                height: MediaQuery.of(context).size.height * 0.3,
                 width: MediaQuery.of(context).size.width * 0.98,
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(30),
@@ -20,44 +20,126 @@ class Testing extends StatelessWidget {
                 child: Padding(
                   padding: const EdgeInsets.all(25.0),
                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       Row(
                         children: [
                           Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text("Hi Edria!",style: TextStyle(
-                                color: Colors.white,
-                                fontFamily: "Itim"
-                                  ,fontSize: 25,fontWeight: FontWeight.bold
-                              ),),
-                              Text("How do you feel today?",style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontFamily: "Itim"
-                              ),)
+                              Text(
+                                "Hi Edria!",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: "Itim",
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                "How do you feel today?",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    fontFamily: "Itim"),
+                              )
                             ],
                           ),
-                          IconButton(onPressed: (){}, icon:Icon(Icons.add_alert_sharp,color: Colors.white,))
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * .35,
+                          ),
+                          Container(
+                            height: MediaQuery.of(context).size.height * .05,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.white.withOpacity(0.5),
+                              ),
+                              child: Icon(Icons.notifications),
+                              onPressed: () {},
+                            ),
+                          ),
                         ],
                       ),
                       TextField(
                         decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: "Search Doctor, Health issues..."
-                        ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15)
+                            ),
+                            filled: true,
+                            fillColor: Colors.white.withOpacity(0.6),
+                            hintText: "Search Doctor, Health issues..."),
                       ),
                       Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          IconButton(onPressed: (){}, icon:Icon(Icons.add_alert_sharp,color: Colors.white,)),
-                          IconButton(onPressed: (){}, icon:Icon(Icons.add_alert_sharp,color: Colors.white,)),
-                          IconButton(onPressed: (){}, icon:Icon(Icons.add_alert_sharp,color: Colors.white,)),
-                          IconButton(onPressed: (){}, icon:Icon(Icons.add_alert_sharp,color: Colors.white,)),
+                          Container(
+                            height: MediaQuery.of(context).size.height * .08,
+                            width: MediaQuery.of(context).size.width*.15,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.white.withOpacity(0.6),
+                              ),
+                              child: Icon(Icons.notifications,
+                              size: 40,
+                              ),
+                              onPressed: () {},
+                            ),
+                          ),
+                          Container(
+                            height: MediaQuery.of(context).size.height * .08,
+                            width: MediaQuery.of(context).size.width*.15,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.white.withOpacity(0.6),
+                              ),
+                              child: Icon(Icons.notifications,
+                                size: 40,
+                              ),
+                              onPressed: () {},
+                            ),
+                          ),
+                          Container(
+                            height: MediaQuery.of(context).size.height * .08,
+                            width: MediaQuery.of(context).size.width*.15,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.white.withOpacity(0.6),
+                              ),
+                              child: Icon(Icons.notifications,
+                                size: 40,
+                              ),
+                              onPressed: () {},
+                            ),
+                          ),
+                          Container(
+                            height: MediaQuery.of(context).size.height * .08,
+                            width: MediaQuery.of(context).size.width*.15,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.white.withOpacity(0.6),
+                              ),
+                              child: Icon(Icons.notifications,
+                                size: 40,
+                              ),
+                              onPressed: () {},
+                            ),
+                          ),Container(
+                            height: MediaQuery.of(context).size.height * .08,
+                            width: MediaQuery.of(context).size.width*.15,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.white.withOpacity(0.6),
+                              ),
+                              child: Icon(Icons.notifications,
+                                size: 40,
+                              ),
+                              onPressed: () {},
+                            ),
+                          ),
                         ],
                       )
                     ],
                   ),
                 ),
-
               ),
             ],
           ),
